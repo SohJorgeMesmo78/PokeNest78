361e61e376579515076bd118d9439ce73f31fb60
SohJorgeMesmo78 <jf78pereira@gmail.com>
2025-03-28 20:02:57 -0300

    adicioanndo segundo tipo as fraquazes de pokemon

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -3,59 +3,8 @@ import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class PokemonService {
-    constructor(private prismaService: PrismaService) {}
+    constructor(private prismaService: PrismaService) { }
 
-    async GetAllPokemons() {
-        const pokemons = await this.prismaService.pokemon.findMany({
-            orderBy: { numDex: 'asc' },
-            select: {
-                numDex: true,
-                nome: true,
-                tipos: {
-                    select: {
-                        tipo: { select: { id: true, nome: true } },
-                        ordem: true,
-                    },
-                    orderBy: { ordem: 'asc' },
-                },
-            },
-        });
-    
-        return pokemons.map(async (pokemon) => {
-            // Buscando as fraquezas baseadas no primeiro tipo
-            const firstTipoId = pokemon.tipos[0]?.tipo.id;
-            const fraquezas = firstTipoId
-                ? await this.prismaService.fraqueza.findMany({
-                      where: {
-                          idAtaque: firstTipoId,
-                      },
-                      select: {
-                          fraqueza: true,
-                          tipoDefesa: {
-                              select: {
-                                  nome: true,
-                              },
-                          },
-                      },
-                  })
-                : [];
-    
-            return {
-                numDex: pokemon.numDex,
-                nome: pokemon.nome,
-                tipos: pokemon.tipos.map((tipo) => ({
-                    id: tipo.tipo.id,
-                    nome: tipo.tipo.nome,
-                    ordem: tipo.ordem,
-                })),
-                fraquezas: fraquezas.map((fraqueza) => ({
-                    tipoAtaque: fraqueza.tipoDefesa.nome,
-                    fraqueza: fraqueza.fraqueza,
-                })),
-            };
-        });
-    }
-    
     async GetPokemonByNumDex(numDex: number) {
         const pokemon = await this.prismaService.pokemon.findUnique({
             where: { numDex },
@@ -71,29 +20,14 @@ export class PokemonService {
                 },
             },
         });
-    
+
         if (!pokemon) {
             throw new UnauthorizedException('Pokémon não encontrado');
         }
-    
-        // Buscando as fraquezas baseadas no primeiro tipo
-        const firstTipoId = pokemon.tipos[0]?.tipo.id;
-        const fraquezas = firstTipoId
-            ? await this.prismaService.fraqueza.findMany({
-                  where: {
-                      idAtaque: firstTipoId,
-                  },
-                  select: {
-                      fraqueza: true,
-                      tipoDefesa: {
-                          select: {
-                              nome: true,
-                          },
-                      },
-                  },
-              })
-            : [];
-    
+
+        const tipoIds = pokemon.tipos.map((t) => t.tipo.id);
+        const fraquezas = await this.calcularFraquezas(tipoIds);
+
         return {
             numDex: pokemon.numDex,
             nome: pokemon.nome,
@@ -102,10 +36,70 @@ export class PokemonService {
                 nome: tipo.tipo.nome,
                 ordem: tipo.ordem,
             })),
-            fraquezas: fraquezas.map((fraqueza) => ({
-                tipoAtaque: fraqueza.tipoDefesa.nome,
-                fraqueza: fraqueza.fraqueza,
-            })),
+            fraquezas,
         };
-    }    
+    }
+
+    async GetAllPokemons() {
+        const pokemons = await this.prismaService.pokemon.findMany({
+            select: {
+                numDex: true,
+                nome: true,
+                tipos: {
+                    select: {
+                        tipo: { select: { id: true, nome: true } },
+                        ordem: true,
+                    },
+                    orderBy: { ordem: 'asc' },
+                },
+            },
+        });
+
+        return await Promise.all(
+            pokemons.map(async (pokemon) => {
+                const tipoIds = pokemon.tipos.map((t) => t.tipo.id);
+                const fraquezas = await this.calcularFraquezas(tipoIds);
+
+                return {
+                    numDex: pokemon.numDex,
+                    nome: pokemon.nome,
+                    tipos: pokemon.tipos.map((tipo) => ({
+                        id: tipo.tipo.id,
+                        nome: tipo.tipo.nome,
+                        ordem: tipo.ordem,
+                    })),
+                    fraquezas,
+                };
+            })
+        );
+    }
+
+    async calcularFraquezas(tipoIds: number[]): Promise<{ tipoAtaque: string; fraqueza: number }[]> {
+        const fraquezasPorTipo = await Promise.all(
+            tipoIds.map((idTipo) =>
+                this.prismaService.fraqueza.findMany({
+                    where: { idDefesa: idTipo },
+                    select: {
+                        fraqueza: true,
+                        tipoAtaque: {
+                            select: { id: true, nome: true },
+                        },
+                    },
+                })
+            )
+        );
+
+        const fraquezasMap = new Map<string, number>();
+
+        fraquezasPorTipo.flat().forEach(({ tipoAtaque, fraqueza }) => {
+            if (fraquezasMap.has(tipoAtaque.nome)) {
+                fraquezasMap.set(tipoAtaque.nome, fraquezasMap.get(tipoAtaque.nome)! * fraqueza);
+            } else {
+                fraquezasMap.set(tipoAtaque.nome, fraqueza);
+            }
+        });
+
+        return Array.from(fraquezasMap, ([tipoAtaque, fraqueza]) => ({ tipoAtaque, fraqueza }));
+    }
+
 }
