f590d03bd61b854e9576bf64704fc0dc89e0192d
SohJorgeMesmo78 <jf78pereira@gmail.com>
2025-03-31 18:31:31 -0300

    corrigindo inicio

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,19 +5,20 @@ export class AppService {
   getHello(): string {
     return `
       <h1>Bem-vindo à API PokeNest!</h1>
-      <p>Esta é a API para gerenciar informações sobre Pokémon. Explore os endpoints para interagir com os dados.</p>
+      <p>Esta é a API para gerenciamento de dados sobre Pokémon. Explore os endpoints abaixo para interagir com os dados.</p>
       
       <h2>Primeiros Passos:</h2>
-      <ol>
-        <li><strong>GET /pokemons</strong> - Obtém uma lista de Pokémon cadastrados.</li>
-        <li><strong>POST /pokemons</strong> - Cria um novo Pokémon.</li>
-        <li><strong>PUT /pokemons/:id</strong> - Atualiza um Pokémon existente.</li>
-        <li><strong>DELETE /pokemons/:id</strong> - Deleta um Pokémon específico.</li>
-      </ol>
+      <ul>
+        <li><strong>GET /pokemon</strong> - Obtém uma lista de Pokémon com paginação (parâmetros: <code>page</code> e <code>limit</code>).</li>
+        <li><strong>GET /pokemon/:identifier</strong> - Obtém informações detalhadas de um Pokémon (por nome ou ID).</li>
+        <li><strong>GET /tipos</strong> - Obtém uma lista de todos os tipos de Pokémon.</li>
+        <li><strong>GET /tipos/:nome</strong> - Obtém as relações de um tipo específico de Pokémon (ex: fraquezas, forças).</li>
+        <li><strong>GET /evolucao/:pokemon</strong> - Obtém a linha evolutiva de um Pokémon específico.</li>
+      </ul>
       
       <h2>Documentação Completa</h2>
-      <p>Consulte a documentação completa para detalhes sobre os endpoints e suas funcionalidades.</p>
-      <p>Veja o código-fonte e contribua para o projeto: 
+      <p>Consulte a documentação completa para mais detalhes sobre como usar a API e seus endpoints.</p>
+      <p>Confira o código-fonte e contribua para o projeto: 
         <a href="https://github.com/SohJorgeMesmo78/PokeNest78" target="_blank">GitHub - PokeNest78</a>
       </p>
     `;
