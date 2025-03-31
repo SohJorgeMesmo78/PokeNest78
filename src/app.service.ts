5fc4dc4595262ae0220918890161d71ba54b50ff
SohJorgeMesmo78 <jf78pereira@gmail.com>
2025-03-31 18:30:06 -0300

    getHello

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,14 +5,19 @@ export class AppService {
   getHello(): string {
     return `
       <h1>Bem-vindo à API PokeNest!</h1>
-      <p>Esta é uma API para gerenciamento de dados sobre Pokémon. Utilize os endpoints abaixo para interagir com a aplicação.</p>
+      <p>Esta é a API para gerenciar informações sobre Pokémon. Explore os endpoints para interagir com os dados.</p>
+      
       <h2>Primeiros Passos:</h2>
-      <ul>
-        <li>Faça requisições GET para obter dados.</li>
-        <li>Envie dados via POST para adicionar novos registros.</li>
-        <li>Utilize PUT ou DELETE para atualizar ou remover dados.</li>
-      </ul>
-      <p>Confira o código-fonte e mais informações em: 
+      <ol>
+        <li><strong>GET /pokemons</strong> - Obtém uma lista de Pokémon cadastrados.</li>
+        <li><strong>POST /pokemons</strong> - Cria um novo Pokémon.</li>
+        <li><strong>PUT /pokemons/:id</strong> - Atualiza um Pokémon existente.</li>
+        <li><strong>DELETE /pokemons/:id</strong> - Deleta um Pokémon específico.</li>
+      </ol>
+      
+      <h2>Documentação Completa</h2>
+      <p>Consulte a documentação completa para detalhes sobre os endpoints e suas funcionalidades.</p>
+      <p>Veja o código-fonte e contribua para o projeto: 
         <a href="https://github.com/SohJorgeMesmo78/PokeNest78" target="_blank">GitHub - PokeNest78</a>
       </p>
     `;
